f6c826b4a1fe8bc9c633d3c899f2025e5e072d99
Andrew3369 <andrewjanosbabos@gmail.com>
2023-09-10 21:36:33 -0400

    hi Spiros

---
+++ main.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://visual_world.tscn")
+
+
+# Quit Button
+func _on_quit_button_pressed():
+	#pass # Replace with function body.
+	get_tree().quit()
